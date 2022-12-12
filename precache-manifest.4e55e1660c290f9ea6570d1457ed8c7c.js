2780d55da656d2deecf35e38e7166298d88ee4cd
danjhkim <danjhkim@gmail.com>
2022-12-12 00:12:51 -0500

    Updates

---
+++ precache-manifest.4e55e1660c290f9ea6570d1457ed8c7c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "658a8db8b0f5d528af23",
+    "url": "/updated-robot-filter-list/static/css/main.33203901.chunk.css"
+  },
+  {
+    "revision": "658a8db8b0f5d528af23",
+    "url": "/updated-robot-filter-list/static/js/main.658a8db8.chunk.js"
+  },
+  {
+    "revision": "f28b4e2ac98b97beb21f",
+    "url": "/updated-robot-filter-list/static/js/1.f28b4e2a.chunk.js"
+  },
+  {
+    "revision": "5b5a1c892fd752d0d8fc",
+    "url": "/updated-robot-filter-list/static/js/runtime~main.5b5a1c89.js"
+  },
+  {
+    "revision": "a97dcae48d148c2a9ccac6b8be827bac",
+    "url": "/updated-robot-filter-list/static/media/raleway-v22-latin-regular.a97dcae4.eot"
+  },
+  {
+    "revision": "7d23a14ed84205e48176f9eff3e96d78",
+    "url": "/updated-robot-filter-list/static/media/raleway-v22-latin-regular.7d23a14e.svg"
+  },
+  {
+    "revision": "131f660715196288a68bd84296ada895",
+    "url": "/updated-robot-filter-list/static/media/raleway-v22-latin-regular.131f6607.woff2"
+  },
+  {
+    "revision": "ce22119ec5a34ef3d200892f0b1c3c0c",
+    "url": "/updated-robot-filter-list/static/media/raleway-v22-latin-regular.ce22119e.woff"
+  },
+  {
+    "revision": "6fbc6996005173c5324ebf739f7ac60c",
+    "url": "/updated-robot-filter-list/static/media/raleway-v22-latin-800.6fbc6996.woff2"
+  },
+  {
+    "revision": "794042eb4733123da66b2713bc5aeadc",
+    "url": "/updated-robot-filter-list/static/media/raleway-v22-latin-regular.794042eb.ttf"
+  },
+  {
+    "revision": "8026f84107cbdadb69d84050be79f5c6",
+    "url": "/updated-robot-filter-list/static/media/raleway-v22-latin-800.8026f841.eot"
+  },
+  {
+    "revision": "44a0c7110fc9a61d4677853bec63173c",
+    "url": "/updated-robot-filter-list/static/media/raleway-v22-latin-800.44a0c711.woff"
+  },
+  {
+    "revision": "bac8a14d49f7af7fa8ab0d040e1886b2",
+    "url": "/updated-robot-filter-list/static/media/raleway-v22-latin-800.bac8a14d.svg"
+  },
+  {
+    "revision": "52c56726aa55db534b446bc33e529a5c",
+    "url": "/updated-robot-filter-list/static/media/raleway-v22-latin-800.52c56726.ttf"
+  },
+  {
+    "revision": "fcfad44c6059986eb3da8f6562b1d65f",
+    "url": "/updated-robot-filter-list/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/updated-robot-filter-list/precache-manifest.4e55e1660c290f9ea6570d1457ed8c7c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/updated-robot-filter-list/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
